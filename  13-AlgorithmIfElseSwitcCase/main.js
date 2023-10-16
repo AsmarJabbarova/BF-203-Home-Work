db6603d5536e23253c54d9d7f211ce6a0edf4a6f
AsmarJabbarova <145671712+AsmarJabbarova@users.noreply.github.com>
2023-10-16 13:05:52 +0400

    Add files via upload

---
+++  13-AlgorithmIfElseSwitcCase/main.js
@@ -0,0 +1,60 @@
+// let eded1 = Number(prompt("Birinci ədədi daxil edin:"));
+// let eded2 = Number(prompt("İkinci ədədi daxil edin:"));
+// let eded3 = Number(prompt("Üçüncü ədədi daxil edin:"));
+
+// if (eded1 + eded2 > eded3 && eded1 + eded3 > eded2 && eded2 + eded3 > eded1) {
+//     console.log("üçbucaqdir.");
+// } else {
+//     console.log("üçbucaq deyil");
+// }
+
+
+
+
+
+// let a = Number(prompt("Birinci tərəfi daxil edin:"));
+// let b = Number(prompt("İkinci tərəfi daxil edin:"));
+// let c = Number(prompt("Üçüncü tərəfi daxil edin:"));
+
+// if (a === b && b === c) {
+//     console.log("bərabərtərəfli üçbucaqdır.");
+// } else {
+//     console.log("bərabərtərəfli üçbucaq deyil.");
+// }
+
+
+
+
+// let eded1 = Number(prompt("Birinci ədədi daxil edin:"));
+// let eded2 = Numbers(prompt("İkinci ədədi daxil edin:"));
+
+// if (eded1 > eded2) {
+//     console.log("Birinci ədəd ən böyükdür.");
+// } else if (eded2 > eded1) {
+//     console.log("İkinci ədəd ən böyükdür.");
+// } else {
+//     console.log("Ədədlər bərabərdir.");
+// }
+
+
+
+
+
+
+// let eded1 = Number(prompt("Birinci ədədi daxil edin:"));
+// let eded2 = Number(prompt("İkinci ədədi daxil edin:"));
+// let eded3 = Number(prompt("Üçüncü ədədi daxil edin:"));
+
+// if (eded1 > eded2 && eded1 > eded3) {
+//     console.log("Birinci ədəd ən böyükdür.");
+// } else if (eded2 > eded1 && eded2 > eded3) {
+//     console.log("İkinci ədəd ən böyükdür.");
+// } else if (eded3 > eded1 && eded3 > eded2) {
+//     console.log("Üçüncü ədəd ən böyükdür.");
+// } else {
+//     console.log("Ədədlər eynidir və ya eyni ədəd daxil edilib.");
+// }
+
+
+
+
